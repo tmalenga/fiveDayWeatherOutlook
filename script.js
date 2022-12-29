611e136c06f9eb8d393717afce748c1f8613a2ca
tmalenga0@gmail.com <tmalenga0@gmail.com>
2022-12-29 16:28:56 -0400

    Add forecast weather data

--- script.js
+++ script.js
@@ -32,5 +32,37 @@ $(function () {
             document.getElementById("current-wind").innerHTML = "<b>Wind speed</b>: " + speed + " mph";
             document.getElementById("current-humidity").innerHTML = "<b>Humidity</b>: "+ humidity + "%";            
         }
+
+        searchBtn.click(function(){
+            let userInput = userIpnut.val();
+            console.log(userInput)
+              fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ userInput +"&units=metric&appid=5a74568b4d5114e452a53cc6921752df")
+                
+                .then(response => response.json())
+                .then(data => {forecastWeather()
+                    console.log(data);
+                    let cityName = data.city.name;
+                    let temp_1 = data.list[0].main.temp;
+                    let humidity_1 = data.list[0].main.humidity + "%";
+                    let speed_1 = data.list[0].wind.speed + "mph";
+                    let icon_1 = data.list[0].weather[0].icon;
+                    console.log(cityName + " "+ temp_1 + " " + humidity_1 + " "+ speed_1 + " " + icon_1);
+
+                    for(i=0; i<40;i+=8){
+                        counter=0;
+                        document.getElementById("temp-"+counter).innerHTML = data.list[0].main.temp;
+                    }
+        
+                })
+
+            function forecastWeather(response){
+                console.log("!!!!!!!!!!!" + response)
+
+            }
+
+     
+        
+
+    })
         
 })
