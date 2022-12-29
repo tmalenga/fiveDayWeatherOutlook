8a29913c96423ea1167915e5e265de762dbd69b9
tmalenga0@gmail.com <tmalenga0@gmail.com>
2022-12-29 15:48:12 -0400

    Fetching data from API

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ let userIpnut = $("#userInp");
 let url = "https://api.openweathermap.org/data/2.5/weather?q=";
 let apiKey = "5a74568b4d5114e452a53cc6921752df";
 let city = document.getElementById("city-name")
+
+$(function () {
+    
+    //searchBtn.addEventListener("click", function() {
+    searchBtn.click(function(){
+      
+      let userInput = userIpnut.val();
+      console.log(userInput)
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ userInput +"&units=metric&appid=5a74568b4d5114e452a53cc6921752df")
+          .then(response =>{return response.json();})
+          .then(console.log(response));     
+             
+                      
+          });
+        
+})
