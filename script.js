f47b4afe4e57126a7afef0d558d88c313075408d
tmalenga0@gmail.com <tmalenga0@gmail.com>
2022-12-29 15:40:00 -0400

    JS variables

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
- console.log("working")
+let searchBtn = $(".search-btn");
+let userIpnut = $("#userInp");
+let url = "https://api.openweathermap.org/data/2.5/weather?q=";
+let apiKey = "5a74568b4d5114e452a53cc6921752df";
+let city = document.getElementById("city-name")
